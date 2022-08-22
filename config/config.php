60d0f8f6ba457d1f2d98f2982df20636fbc5f832
MallardDuck <self@danpock.me>
2022-08-22 23:29:30 +0000

    remove unused docs code

--- config/config.php
+++ config/config.php
@@ -4,19 +4,17 @@ declare(strict_types=1);
 
 use BladeUI\Icons\BladeIconsServiceProvider;
 use Codeat3\BladeFileIcons\BladeFileIconsServiceProvider;
-use Kickflip\Providers\KickflipRouterNavServiceProvider;
 use MallardDuck\BladeBoxicons\BladeBoxiconsServiceProvider;
 use MallardDuck\BladeEmojiIcons\BladeEmojiIconsServiceProvider;
 
 return [
     'baseUrl' => 'http://kickflip-docs.test/',
     'production' => false,
-    'siteName' => 'Kickflip',
+    'siteName' => 'Kickflip CLI',
     'siteDescription' => 'Static site generation based on Laravel Zero',
 
     // Composer packages with laravel providers to load
     'providePackages' => [
-        KickflipRouterNavServiceProvider::class,
         BladeIconsServiceProvider::class,
         BladeFileIconsServiceProvider::class,
         BladeBoxiconsServiceProvider::class,
